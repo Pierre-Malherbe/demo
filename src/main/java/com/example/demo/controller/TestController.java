3b28994d1d12ea67490ca487a99929ee309afc18
Pierre Malherbe <pierre.malherbe6@gmail.com>
2019-03-13 20:10:10 +0100

    Change text 2

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class TestController {
 
     @GetMapping("/")
-    public String trest() {
-        return "Hello from my Java Project ! ";
+    public String trest() throws UnknownHostException {
+        return "Hello from my Java Project !  Notre Hostname actuellement la est : " + InetAddress.getLocalHost().getHostName();
     }
 }
